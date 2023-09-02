774be592dfa2158942f948cd65d41e1be155c13d
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-03 01:09:48 +0300

    Add files via upload

---
+++ Basics/Dice Roll/Dice Roll.cs
@@ -0,0 +1,5 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine("The number of the rolled dice is: "+roll);
