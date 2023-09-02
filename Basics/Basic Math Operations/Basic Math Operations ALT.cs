d55802f86f2db628e06a4500ccac74e95b6c93e0
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-03 00:06:14 +0300

    Add files via upload

---
+++ Basics/Basic Math Operations/Basic Math Operations ALT.cs
@@ -0,0 +1,14 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+int first_number = 13;
+int second_number=5;
+int sum = first_number + second_number;
+int difference = first_number - second_number;
+int product = first_number * second_number;
+decimal quotient = (decimal) first_number / (decimal)second_number;
+
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
