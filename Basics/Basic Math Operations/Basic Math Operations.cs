8eeee1a3d344b07e9100fa77f6da2e2cdc582ecc
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-03 00:01:36 +0300

    Add files via upload

---
+++ Basics/Basic Math Operations/Basic Math Operations.cs
@@ -0,0 +1,14 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+decimal first_number = 10m;
+decimal second_number=5m;
+decimal sum = first_number + second_number;
+decimal difference = first_number - second_number;
+decimal product = first_number * second_number;
+decimal quotient = first_number / second_number;
+
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
