1fee2a8b39b8bd6643be58d9d1b2aa91800b7666
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-03 11:13:36 +0300

    Add files via upload

---
+++ Basics/Random Number Generator/Random Number Generator.cs
@@ -0,0 +1,5 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+Random randomnumber = new Random();
+int randnum = randomnumber.Next();
+Console.WriteLine("Your random number is: "+randnum);
