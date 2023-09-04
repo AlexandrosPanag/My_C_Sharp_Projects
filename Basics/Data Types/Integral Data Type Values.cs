abfd249b41fe6ee0ae61ae74c11da7006f000e14
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-04 20:19:02 +0300

    Add files via upload

---
+++ Basics/Data Types/Integral Data Type Values.cs
@@ -0,0 +1,16 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"ushort  : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"uint    : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+Console.WriteLine($"ulong   : {ulong.MinValue} to {ulong.MaxValue}");
+Console.WriteLine($"float   : {float.MinValue} to {float.MaxValue}");
+Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue}");
+Console.WriteLine($"decimal   : {decimal.MinValue} to {decimal.MaxValue}");
+Console.WriteLine($"char   : {char.MinValue} to {char.MaxValue}");
+Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue}");
