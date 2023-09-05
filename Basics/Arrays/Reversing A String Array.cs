7c344348842121c608ab0ca5717d27f1fd7baaf6
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-06 00:31:07 +0300

    Add files via upload

---
+++ Basics/Arrays/Reversing A String Array.cs
@@ -0,0 +1,6 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+string value = "open sesame";
+char[] valueArray = value.ToCharArray(); //turn the string into a char array
+Array.Reverse(valueArray); //reverse the char array
+Console.WriteLine(valueArray); //print the array
