5b0c4d1da93d8aa14b46109f8f6c9412fe659087
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-06 00:45:03 +0300

    Add files via upload

---
+++ Basics/String formatting/Composite Formatting.cs
@@ -0,0 +1,6 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+string first = "Open";
+string second = "Sesame";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
