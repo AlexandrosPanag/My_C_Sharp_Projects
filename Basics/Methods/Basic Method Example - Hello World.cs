56934b656515c9b44cff50c6696df501e5d4dc21
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-06 15:31:27 +0300

    Add files via upload

---
+++ Basics/Methods/Basic Method Example - Hello World.cs
@@ -0,0 +1,11 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+Console.WriteLine("Before calling a method");
+SayHello();
+Console.WriteLine("After calling a method");
+
+void SayHello()
+{
+    Console.WriteLine("Hello World!");
+}
+ 
