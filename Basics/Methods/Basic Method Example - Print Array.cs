0bcd89ffc7241c8e4584cee2c187862a0ef0bee4
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-06 15:33:59 +0300

    Add files via upload

---
+++ Basics/Methods/Basic Method Example - Print Array.cs
@@ -0,0 +1,15 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+int[] a = {1,2,3,4,5};
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+
+void PrintArray()
+{
+    foreach (int x in a)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
