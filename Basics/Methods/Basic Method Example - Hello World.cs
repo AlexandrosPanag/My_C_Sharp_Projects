f1f5c8c5de92c3e2f8536c8afd7055890c7474f5
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-06 15:32:33 +0300

    Update Basic Method Example - Hello World.cs

--- Basics/Methods/Basic Method Example - Hello World.cs
+++ Basics/Methods/Basic Method Example - Hello World.cs
@@ -1,8 +1,7 @@
 //By Alexandros Panagiotakopoulos - alexandrospanag.github.io
 
-Console.WriteLine("Before calling a method");
 SayHello();
-Console.WriteLine("After calling a method");
+
 
 void SayHello()
 {
