b41e77c7e97208b94c9d583c7aae3af6b59983cd
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-06 14:41:53 +0300

    Add files via upload

---
+++ Basics/String formatting/String Remove.cs
@@ -0,0 +1,5 @@
+//By Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+string data = "12345John  Doe             5000  3  ";
+string updatedData = data.Remove(5, 20); //remove everything from the 5th string character until the 20th
+Console.WriteLine(updatedData);
