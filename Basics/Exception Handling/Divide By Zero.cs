4e3bada62fd9a701672e2ec5511d96934690123b
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-07 12:15:54 +0300

    Add files via upload

---
+++ Basics/Exception Handling/Divide By Zero.cs
@@ -0,0 +1,28 @@
+// Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+try // try code block - code that may generate an exception
+{
+    Process1();
+}
+catch // catch code block - code to handle an exception
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exitting the program");
+
+static void Process1()
+{
+    WriteMessage();
+}
+
+static void WriteMessage()
+{
+    double float1 = 100.0;
+    double float2 = 0.0;
+    int number1 = 100;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
