a5f838560674c09ce4ee2a073ad70eb5672b6fd4
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-09-07 14:27:57 +0300

    Add files via upload

---
+++ Basics/User Input/User Input.cs
@@ -0,0 +1,10 @@
+// Alexandros Panagiotakopoulos - alexandrospanag.github.io
+
+// Type your anything and press enter
+Console.WriteLine("Type anything! :");
+
+// Read the User Input
+string myword = Console.ReadLine();
+
+// Print the value 
+Console.WriteLine("You have typed: " + myword);
